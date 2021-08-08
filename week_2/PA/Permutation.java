15e52456aec839a85331f788a6b0650f741ef31a
Tang <m18852083652@163.com>
2021-08-08 06:04:55 -0700

    memory bonus get!

--- week_2/PA/Permutation.java
+++ week_2/PA/Permutation.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
 	public static void main(String[] args) {
@@ -6,18 +7,43 @@ public class Permutation {
 		if (k == 0) {
             return;
         }
+		/*
+		String[] arr = new String[k];
+		int random_id;
+		for (int i = 0; !StdIn.isEmpty(); i++) {
+			String a = StdIn.readString();
+			if (i < k)
+				arr[i] = a;
+			else {
+				random_id = StdRandom.uniform(i+1); // [0, 1, ...i]
+				if (random_id < k)
+					arr[random_id] = a;
+			}
+		}
+		for (String s : arr)
+			System.out.println(s);
+		*/
+		
 		var queue = new RandomizedQueue<String>();
+		int i = 0;
+		int random_id;
 		while (!StdIn.isEmpty()) {
 			String a = StdIn.readString();
-			queue.enqueue(a);
+			if (i < k) {
+				queue.enqueue(a);
+			}
+			else {
+				random_id = StdRandom.uniform(i+1); // [0, 1, ...i]
+                if (random_id < k) {
+					queue.dequeue();
+					queue.enqueue(a);
+				}
+			}
+			i++;
 		}
 		
-		int i = 0;
 		for (String s : queue) {
 			System.out.println(s);
-			i++;
-			if (i == k)
-				break;
 		}
 	}
 }
