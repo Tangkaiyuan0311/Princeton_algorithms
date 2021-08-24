feacc84d1e9d079761fe263cc1fc339839d05d2a
Tang <m18852083652@163.com>
2021-08-24 09:54:51 -0700

    2021.8.25

--- week_4/PA/Solver.java
+++ week_4/PA/Solver.java
@@ -1,6 +1,8 @@
 import java.util.Comparator;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class Solver {
 
@@ -11,8 +13,8 @@ public class Solver {
 	
 	// search node data type
 	private class Node {
-		public static hamming_cmp = new hamming_order();
-		public static manhattan_cmp = new manhattan_order();
+		// public final hamming_order hamming_cmp = new hamming_order();
+		// public final manhattan_order manhattan_cmp = new manhattan_order();
 		public final Board board;
 		public final int moves;
 		public final Node prev;
@@ -25,20 +27,61 @@ public class Solver {
 			this.hamming = board.hamming();
 			this.manhattan = board.manhattan();
 		}
-		private class hamming_order implements Comparator<Node> {
-			public int compare(Node x, Node y) {
-				return (x.hamming+x.moves) - (y.hamming+y.moves);
-			}
-		}
-		private class manhattan_order implements Comparator<Node> {
-            public int compare(Node x, Node y) {
-                return (x.manhattan+x.moves) - (y.manhattan+y.moves);
-            }
-        }
 	}
+	private class hamming_order implements Comparator<Node> {
+        public int compare(Node x, Node y) {
+            return (x.hamming+x.moves) - (y.hamming+y.moves);
+        }
+    }
+    private class manhattan_order implements Comparator<Node> {
+		public int compare(Node x, Node y) {
+			return (x.manhattan+x.moves) - (y.manhattan+y.moves);
+        }
+    }
 
+	private int process(MinPQ<Node> pq) {
+		Node current_node = pq.delMin();
+		if (current_node.board.isGoal()) {
+			goal = current_node;
+			return 1;
+		}
+		else
+			for (var neighbor : current_node.board.neighbors()) {
+				if ((current_node.prev != null) && (neighbor.equals(current_node.prev.board)))
+					continue;
+				else
+					pq.insert(new Node(neighbor, current_node.moves+1, current_node));
+			}
+		return 0;
+	}
+	
     // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial)
+    public Solver(Board initial) {
+		goal = null;
+		solvable = false;
+		var pq = new MinPQ<Node>(new hamming_order());
+		var pq_twin = new MinPQ<Node>(new hamming_order());
+		pq.insert(new Node(initial, 0, null));
+		pq_twin.insert(new Node(initial, 0, null));
+
+		int flag;
+		while (true) {
+			if (!pq.isEmpty()) {
+				flag = process(pq);
+				if (flag == 1) {
+					solvable = true;
+					break;
+				}
+			}
+			if (!pq_twin.isEmpty()) {
+                flag = process(pq_twin);
+                if (flag == 1) {
+                    solvable = false;
+                    break;
+                }
+            }
+		}
+	}
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
@@ -53,8 +96,21 @@ public class Solver {
 			return -1;
 	}
 
+	
     // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution()
+    public Iterable<Board> solution() {
+		if (solvable) {
+			var s = new Stack<Board>();
+			Node x = goal;
+			while (x != null) {
+				s.push(x.board);
+				x = x.prev;
+			}
+			return s;
+		}
+		else
+			return null;
+	}
 
     // test client (see below) 
     public static void main(String[] args) {
